8f8381e9f3cecb0f0b685829deec0623dc6cf8e1
Cody Ray Hoeft <crhoeft@gmail.com>
2021-12-01 22:11:41 -0800

    Day 2: Some refactoring - the sealed classes didn't help much anyway

--- src/test/kotlin/Day02.kt
+++ src/test/kotlin/Day02.kt
@@ -2,70 +2,50 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 val EXAMPLE_DATA = listOf(
-    Forward(5),
-    Down(5),
-    Forward(8),
-    Up(3),
-    Down(8),
-    Forward(2),
+    Command("forward", 5),
+    Command("down", 5),
+    Command("forward", 8),
+    Command("up", 3),
+    Command("down", 8),
+    Command("forward", 2),
 )
 
-data class SubmarineState(val aim: Int, val depth: Int, val horizontal: Int)
-
-sealed class Command {
+data class Submarine(val aim: Int, val depth: Int, val horizontal: Int, val handlesAim: Boolean) {
     companion object {
-        fun parseAll(commandStrings: List<String>): List<Command> = commandStrings.map {
-            val (type, distanceStr) = it.split(' ', limit = 2)
-            val distance = distanceStr.toInt()
-            when (type) {
-                "forward" -> Forward(distance)
-                "down" -> Down(distance)
-                "up" -> Up(distance)
-                else -> error("Bad command type")
-            }
-        }
+        val part1 = Submarine(0, 0, 0, false)
+        val part2 = Submarine(0, 0, 0, true)
     }
 
-    abstract fun simulate(coords: SubmarineState, part2: Boolean): SubmarineState
+    fun move(changeAim: Int = 0, changeDepth: Int = 0, changeHorizontal: Int = 0): Submarine =
+        Submarine(aim + changeAim, depth + changeDepth, horizontal + changeHorizontal, handlesAim)
+
+    fun simulate(command: Command): Submarine = when (command.type) {
+        "forward" -> move(changeDepth = aim * command.movement, changeHorizontal = command.movement)
+        "up" -> if (handlesAim) move(changeAim = -command.movement) else move(changeDepth = -command.movement)
+        "down" -> if (handlesAim) move(changeAim = command.movement) else move(changeDepth = command.movement)
+        else -> error("bad command")
+    }
+
+    fun simulate(commands: List<Command>): Submarine =
+        commands.fold(this) { currentCoords, command -> currentCoords.simulate(command) }
 }
 
-fun List<Command>.simulate(coords: SubmarineState, part2: Boolean): SubmarineState =
-    fold(coords) { currentCoords, command -> command.simulate(currentCoords, part2) }
-
-data class Forward(val distance: Int) : Command() {
-    override fun simulate(coords: SubmarineState, part2: Boolean): SubmarineState =
-        SubmarineState(
-            aim = coords.aim,
-            depth = coords.depth + coords.aim * distance,
-            horizontal = coords.horizontal + distance
-        )
-}
-
-data class Down(val distance: Int) : Command() {
-    override fun simulate(coords: SubmarineState, part2: Boolean): SubmarineState = if (part2)
-        SubmarineState(aim = coords.aim + distance, depth = coords.depth, horizontal = coords.horizontal)
-    else
-        SubmarineState(aim = coords.aim, depth = coords.depth + distance, horizontal = coords.horizontal)
-
-}
-
-data class Up(val distance: Int) : Command() {
-    override fun simulate(coords: SubmarineState, part2: Boolean): SubmarineState = if (part2)
-        SubmarineState(aim = coords.aim - distance, depth = coords.depth, horizontal = coords.horizontal)
-    else
-        SubmarineState(aim = coords.aim, depth = coords.depth - distance, coords.horizontal)
+data class Command(val type: String, val movement: Int) {
+    companion object {
+        fun parseAll(commandStrings: List<String>): List<Command> = commandStrings.map {
+            it.split(' ', limit = 2).let { (type, distanceStr) -> Command(type, distanceStr.toInt()) }
+        }
+    }
 }
 
 class Day02 {
     @Test
     fun main() {
         val commands = Command.parseAll(readPuzzleInputLines("Day02"))
-        val part1 = commands.simulate(SubmarineState(0, 0, 0), part2 = false)
-            .let { it.depth * it.horizontal }
+        val part1 = Submarine.part1.simulate(commands).let { it.depth * it.horizontal }
         println("Day 2, Part 1: $part1")
         assertEquals(2150351, part1)
-        val part2 = commands.simulate(SubmarineState(0, 0, 0), part2 = true)
-            .let { it.depth * it.horizontal }
+        val part2 = Submarine.part2.simulate(commands).let { it.depth * it.horizontal }
         println("Day 2, Part 2: $part2")
         assertEquals(1842742223, part2)
     }
@@ -88,11 +68,13 @@ class Day02 {
 
     @Test
     fun `test solution`() {
-        val exampleCoords = EXAMPLE_DATA.simulate(SubmarineState(0, 0, 0), part2 = false)
+        assertEquals(1, Submarine.part2.move(1).aim)
+        assertEquals(1, Submarine.part2.simulate(Command("down", 1)).aim)
+        val exampleCoords = Submarine.part1.simulate(EXAMPLE_DATA)
         assertEquals(15, exampleCoords.horizontal)
         assertEquals(10, exampleCoords.depth)
         assertEquals(150, exampleCoords.depth * exampleCoords.horizontal)
-        val exampleCoords2 = EXAMPLE_DATA.simulate(SubmarineState(0, 0, 0), part2 = true)
+        val exampleCoords2 = Submarine.part2.simulate(EXAMPLE_DATA)
         assertEquals(15, exampleCoords2.horizontal)
         assertEquals(60, exampleCoords2.depth)
         assertEquals(900, exampleCoords2.depth * exampleCoords2.horizontal)
