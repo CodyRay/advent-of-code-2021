efe2e7e68f060f1c79986ae56568a91f145472dc
Cody Ray Hoeft <crhoeft@gmail.com>
2021-12-13 18:27:04 -0800

    Day 12: Part 2: Initial solution

--- src/test/kotlin/Day12.kt
+++ src/test/kotlin/Day12.kt
@@ -60,25 +60,42 @@ fun parse(input: List<String>): CaveSystem = input.map { it.split('-') }
 
 data class Cave(val name: String, val isBig: Boolean, val paths: List<String>)
 
-fun enumeratePaths(caveSystem: CaveSystem, path: Path): List<Path> =
+fun enumeratePaths(
+    caveSystem: CaveSystem,
+    validCave: (CaveSystem, Path, String) -> Boolean,
+    path: Path = listOf("start")
+): List<Path> =
     if (path.last() == "end") {
         listOf(path)
     } else {
         caveSystem[path.last()]!!.paths
-            .filter { caveSystem[it]!!.isBig || it !in path }
-            .flatMap { enumeratePaths(caveSystem, path.plus(it)) }
+            .filter { validCave(caveSystem, path, it) }
+            .flatMap { enumeratePaths(caveSystem, validCave, path.plus(it)) }
     }
+
+private fun isValidSingleVisitCave(caveSystem: CaveSystem, path: Path, cave: String) =
+    caveSystem[cave]!!.isBig || cave !in path
+
+private fun isValidTwoVisitCave(caveSystem: CaveSystem, path: Path, cave: String): Boolean {
+    if (caveSystem[cave]!!.isBig || cave !in path) {
+        return true
+    }
+    if (path.map { caveSystem[it]!! }.filter { !it.isBig }.groupBy { it.name }.all { it.value.count() < 2 }) {
+        return !(cave == "start" || cave == "end")
+    }
+    return false
+}
 
 class Day12 {
     @Test
     fun main() {
         val parsed = parse(readPuzzleInputLines("Day12"))
-        val part1 = enumeratePaths(parsed, listOf("start")).count()
+        val part1 = enumeratePaths(parsed, ::isValidSingleVisitCave).count()
         println("Day 12, Part 1: $part1")
         assertEquals(3450, part1)
-        val part2 = 2
+        val part2 = enumeratePaths(parsed, ::isValidTwoVisitCave).count()
         println("Day 12, Part 2: $part2")
-        // assertEquals(, part2)
+        assertEquals(96528, part2)
     }
 
     @Test
@@ -88,8 +105,11 @@ class Day12 {
 
     @Test
     fun `test solution`() {
-        assertEquals(10, enumeratePaths(parse(example1), listOf("start")).count())
-        assertEquals(19, enumeratePaths(parse(example2), listOf("start")).count())
-        assertEquals(226, enumeratePaths(parse(example3), listOf("start")).count())
+        assertEquals(10, enumeratePaths(parse(example1), ::isValidSingleVisitCave).count())
+        assertEquals(19, enumeratePaths(parse(example2), ::isValidSingleVisitCave).count())
+        assertEquals(226, enumeratePaths(parse(example3), ::isValidSingleVisitCave).count())
+        assertEquals(36, enumeratePaths(parse(example1), ::isValidTwoVisitCave).count())
+        assertEquals(103, enumeratePaths(parse(example2), ::isValidTwoVisitCave).count())
+        assertEquals(3509, enumeratePaths(parse(example3), ::isValidTwoVisitCave).count())
     }
 }
