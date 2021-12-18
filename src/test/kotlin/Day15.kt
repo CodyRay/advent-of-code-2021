a9335ce87af1e5c57a1a01df5c61c1df363d99fa
Cody Ray Hoeft <crhoeft@gmail.com>
2021-12-17 20:17:39 -0800

    Day 15: Incomplete Solution
    I couldn't figure this one out, time to seek out help

--- src/test/kotlin/Day15.kt
+++ src/test/kotlin/Day15.kt
@@ -5,6 +5,7 @@ package day15
 import org.junit.Test
 import readPuzzleInputLines
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 typealias RiskMap = List<List<Int>>
 
@@ -16,15 +17,14 @@ data class Point(val map: RiskMap, val x: Int, val y: Int) {
         return "(${x},${y})"
     }
 }
-//data class RiskPath(val visited: List<Point>, val seen: List<Point>) {
-//
-//}
+
+data class RiskPath(val seen: Set<Point>, val lastPoint: Point)
 
 fun Point.directions(): Set<Point> = buildSet {
     if ((y - 1) in map.indices) add(Point(map, x, y - 1))
     if ((x - 1) in map[y].indices) add(Point(map, x - 1, y))
-    if ((x + 1) in map[y].indices) add(Point(map, x + 1, y))
     if ((y + 1) in map.indices) add(Point(map, x, y + 1))
+    if ((x + 1) in map[y].indices) add(Point(map, x + 1, y))
 }
 
 fun <T> MutableList<Pair<Int, T>>.priorityEnqueue(value: Pair<Int, T>) = this.also {
@@ -36,15 +36,175 @@ fun <T> MutableList<Pair<Int, T>>.priorityEnqueueAll(values: List<Pair<Int, T>>)
 }
 
 fun findLowestRiskPath(map: RiskMap, start: Point = map.start()): Int {
-    val queue = mutableListOf(Pair(0, start))
-    while (queue.first().second != map.end()) {
+    val queue = mutableListOf(Pair(0, RiskPath(setOf(start), start)))
+    while (queue.first().second.lastPoint != map.end()) {
         val (riskLevel, point) = queue.removeFirst()
-        queue.priorityEnqueueAll(point.directions()
+        val visiblePoints = point.lastPoint.directions()
+        queue.priorityEnqueueAll(visiblePoints
+            .filter { it !in point.seen }
             .map {
-                Pair(riskLevel + map[it], it)
+                Pair(riskLevel + map[it], RiskPath(point.seen.union(visiblePoints), it))
             })
     }
     return queue.removeFirst().first
+}
+
+fun memoizedFindLowestRiskPath(map: RiskMap, start: Point = map.start()): Int {
+    val memory = mutableMapOf(map.end() to 0)
+
+    fun lowestRisk(start: Point): Pair<Int, Int> {
+        val queue = mutableListOf(Pair(0, RiskPath(setOf(start), start)))
+        var iterations = 0
+        while (queue.first().second.lastPoint !in memory) {
+            iterations += 1
+            val (riskLevel, point) = queue.removeFirst()
+            val adj = point.lastPoint.directions().filter { it !in point.seen }.sortedByDescending { it.map[it] }
+
+            queue.priorityEnqueueAll(adj
+                .map {
+                    Pair(riskLevel + map[it] + (memory[it] ?: 0), RiskPath(point.seen.union(adj), it))
+                })
+        }
+        return Pair(queue.removeFirst().first, iterations)
+    }
+
+    val populateMemoryQueue = map.end().directions().sortedByDescending { it.map[it] }.toMutableList()
+    while (populateMemoryQueue.isNotEmpty()) {
+        val point = populateMemoryQueue.removeLast()
+        if (point !in memory) {
+            val (riskLevel, iterations) = lowestRisk(point)
+            memory[point] = riskLevel
+            assertNotEquals(0, memory[point])
+            println("$point to ${memory[point]} in $iterations iterations")
+            populateMemoryQueue.addAll(point.directions().sortedByDescending { it.map[it] })
+        }
+    }
+    return memory[map.start()]!!
+}
+
+fun altMemoizedFindLowestRiskPath(map: RiskMap, start: Point = map.start()): Int {
+    val memory = mutableMapOf(map.end() to 0)
+
+    fun lowestRisk(start: Point): Triple<Int, List<Point>, Int> {
+        val queue = mutableListOf(Pair(0, listOf(start)))
+        var iterations = 0
+        while (queue.first().second.last() !in memory) {
+            iterations += 1
+            val (riskLevel, point) = queue.removeFirst()
+            val adj = point.last().directions().filter { it !in point }
+
+            queue.priorityEnqueueAll(adj
+                .map {
+                    Pair(riskLevel + map[it] + (memory[it] ?: 0), point.plus(it))
+                })
+        }
+        val (riskLevel, path) = queue.removeFirst()
+        return Triple(riskLevel, path, iterations)
+    }
+
+    val populateMemoryQueue = map.end().directions().toMutableList()
+
+    fun record(point: Point, riskLevel: Int, iterations: Int) {
+        memory[point] = riskLevel
+        assertNotEquals(0, memory[point])
+        println("$point to ${memory[point]} in $iterations iterations")
+        populateMemoryQueue.addAll(point.directions())
+    }
+
+    while (populateMemoryQueue.isNotEmpty()) {
+        val point = populateMemoryQueue.removeFirst()
+        if (point !in memory) {
+            var (riskLevel, path, iterations) = lowestRisk(point)
+            do {
+                record(path.first(), riskLevel, iterations)
+                iterations = 0
+                riskLevel -= map[path.get(1)]
+                path = path.drop(1)
+            } while (path.size > 1)
+        }
+    }
+    return memory[map.start()]!!
+}
+
+fun finalAttempt(map: RiskMap): Int {
+    val memory = mutableMapOf(map.end() to 0)
+
+    fun lowestRisk(start: Point): Triple<Int, List<Point>, Int> {
+        val queue = mutableListOf(Triple(0, 0, listOf(start)))
+        var minimum = Triple(Int.MAX_VALUE, Int.MAX_VALUE - 10, listOf<Point>())
+        var iterations = 0
+        while (queue.isNotEmpty()) {
+            iterations += 1
+            val (riskLevel, pointCount, points) = queue.removeLast()
+            if (riskLevel < minimum.first && pointCount < (minimum.second + 10)) {
+                if (points.last() in memory) {
+                    minimum = Triple(riskLevel, pointCount, points)
+                } else {
+                    queue.addAll(points.last()
+                        .directions()
+                        .filter { it !in points }
+                        // .sortedByDescending { it.map[it] }
+                        .map { Triple(riskLevel + map[it] + (memory[it] ?: 0), pointCount + 1, points.plus(it)) })
+                }
+            }
+        }
+        return Triple(minimum.first, minimum.third, iterations)
+    }
+
+    val populateMemoryQueue = map.end().directions().toMutableList()
+
+    fun record(point: Point, riskLevel: Int, iterations: Int) {
+        memory[point] = riskLevel
+        assertNotEquals(0, memory[point])
+        println("$point to ${memory[point]} in $iterations iterations")
+        populateMemoryQueue.addAll(point.directions())
+    }
+
+    while (populateMemoryQueue.isNotEmpty()) {
+        val point = populateMemoryQueue.removeFirst()
+        if (point !in memory) {
+            var (riskLevel, path, iterations) = lowestRisk(point)
+            do {
+                record(path.first(), riskLevel, iterations)
+                iterations = 0
+                riskLevel -= map[path[1]]
+                path = path.drop(1)
+            } while (path.size > 1)
+        }
+    }
+    return memory[map.start()]!!
+}
+
+fun npMemoization(map: RiskMap): Int {
+    val success = mutableMapOf(map.end() to 0)
+    val deadEnds = mutableSetOf<Pair<Point, Point>>()
+
+    val populateMemoryQueue = map.end().directions().toMutableList()
+    while (populateMemoryQueue.isNotEmpty()) {
+        val start = populateMemoryQueue.removeFirst()
+        if (start in success) {
+            continue
+        }
+        val queue = mutableListOf(Pair(0, listOf(start)))
+        while (queue.first().second.last() !in success) {
+            val (riskLevel, point) = queue.removeFirst()
+            val visiblePoints = point.last().directions()
+            queue.priorityEnqueueAll(visiblePoints
+                .filter { it !in point }
+                .filter { Pair(point.last(), it) !in deadEnds }
+                .map {
+                    Pair(riskLevel + map[it] + (success[it] ?: 0), point.plus(it))
+                })
+        }
+        success[start] = queue.first().first
+        queue.drop(1).forEach { (_, deadPath) ->
+            deadPath.zipWithNext().forEach {
+                deadEnds.add(it)
+            }
+        }
+        populateMemoryQueue.addAll(start.directions())
+    }
+    return success[map.start()]!!
 }
 
 fun List<String>.parse(): RiskMap = map { line -> line.map { it.digitToInt() } }
@@ -54,7 +214,7 @@ class Day14 {
     @Test
     fun main() {
         val parsed = readPuzzleInputLines("Day15").parse()
-        val part1 = findLowestRiskPath(parsed)
+        val part1 = finalAttempt(parsed)
         println("Day 15, Part 1: $part1")
         //assertEquals(, part1)
         val part2 = 2
@@ -163,6 +323,6 @@ class Day14 {
             listOf(Pair(0, ""), Pair(0, ""), Pair(3, ""), Pair(4, ""), Pair(5, ""), Pair(6, "")),
             queue.priorityEnqueue(Pair(6, ""))
         )
-        assertEquals(40, findLowestRiskPath(exampleX))
+        assertEquals(40, finalAttempt(exampleX))
     }
 }
