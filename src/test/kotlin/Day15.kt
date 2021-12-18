759adfc2d5f6880b8c4ca9988763842b1411abc8
Cody Ray Hoeft <crhoeft@gmail.com>
2021-12-17 22:21:46 -0800

    Day 15: Part 2
    TIL Don't try to make a priority queue out of a list and don't take the hash code of arrays over and over again
    
    Thank you Todd Ginsberg for helping me see the error of my ways https://todd.ginsberg.com/post/advent-of-code/2021/day15/

--- src/test/kotlin/Day15.kt
+++ src/test/kotlin/Day15.kt
@@ -4,46 +4,41 @@ package day15
 
 import org.junit.Test
 import readPuzzleInputLines
+import java.util.*
 import kotlin.test.assertEquals
 
 typealias RiskMap = List<List<Int>>
 
 operator fun RiskMap.get(point: Point) = this[point.y][point.x]
-fun RiskMap.start() = Point(this, 0, 0)
-fun RiskMap.end() = Point(this, first().lastIndex, lastIndex)
-data class Point(val map: RiskMap, val x: Int, val y: Int) {
-    override fun toString(): String {
-        return "(${x},${y})"
-    }
-}
 
-fun Point.directions(): Set<Point> = buildSet {
-    if ((y - 1) in map.indices) add(Point(map, x, y - 1))
-    if ((x - 1) in map[y].indices) add(Point(map, x - 1, y))
-    if ((y + 1) in map.indices) add(Point(map, x, y + 1))
-    if ((x + 1) in map[y].indices) add(Point(map, x + 1, y))
-}
+data class Point(val x: Int, val y: Int)
 
-fun <T> MutableList<Pair<Int, T>>.priorityEnqueue(value: Pair<Int, T>) = this.also {
-    add(binarySearch { it.first - value.first }.let { if (it < 0) -(it + 1) else it }, value)
-}
+fun RiskMap.jumbo() = (0..4).flatMap { yi -> this.map { (0..4).flatMap { xi -> it.jumboIncrement(yi + xi) } } }
 
-fun <T> MutableList<Pair<Int, T>>.priorityEnqueueAll(values: List<Pair<Int, T>>) = this.also {
-    values.forEach { priorityEnqueue(it) }
+fun List<Int>.jumboIncrement(increment: Int) = map { (it - 1 + increment) % 9 + 1 }
+
+fun Point.directions(map: RiskMap): List<Point> = buildList {
+    if ((y - 1) in map.indices) add(Point(x, y - 1))
+    if ((x - 1) in map[y].indices) add(Point(x - 1, y))
+    if ((y + 1) in map.indices) add(Point(x, y + 1))
+    if ((x + 1) in map[y].indices) add(Point(x + 1, y))
 }
 
 fun dijkstras(map: RiskMap): Int {
-    val queue = mutableListOf(Pair(0, map.start()))
+    val end = Point(map.first().lastIndex, map.lastIndex)
+    val priorityQueue = PriorityQueue<Pair<Int, Point>>(11, compareBy { it.first })
+    priorityQueue.add(Pair(0, Point(0, 0)))
     val visited = mutableSetOf<Point>()
-    while (queue.first().second != map.end()) {
-        val (riskLevel, point) = queue.removeFirst()
+    while (priorityQueue.peek().second != end) {
+        val (riskLevel, point) = priorityQueue.poll()
         if (point !in visited) {
-            queue.priorityEnqueueAll(point.directions()
-                .map { Pair(riskLevel + map[it], it) })
+            point.directions(map)
+                .map { Pair(riskLevel + map[it], it) }
+                .forEach { priorityQueue.add(it) }
             visited.add(point)
         }
     }
-    return queue.first().first
+    return priorityQueue.peek().first
 }
 
 fun List<String>.parse(): RiskMap = map { line -> line.map { it.digitToInt() } }
@@ -56,24 +51,90 @@ class Day14 {
         val part1 = dijkstras(parsed)
         println("Day 15, Part 1: $part1")
         assertEquals(717, part1)
-        val part2 = 2
+        val part2 = dijkstras(parsed.jumbo())
         println("Day 15, Part 2: $part2")
-        //assertEquals(, part2)
+        assertEquals(2993, part2)
     }
 
     @Test
-    fun `test priority queue`() {
-        val queue = mutableListOf(Pair(5, ""))
-        assertEquals(listOf(Pair(0, ""), Pair(5, "")), queue.priorityEnqueue(Pair(0, "")))
-        assertEquals(listOf(Pair(0, ""), Pair(0, ""), Pair(5, "")), queue.priorityEnqueue(Pair(0, "")))
-        assertEquals(listOf(Pair(0, ""), Pair(0, ""), Pair(3, ""), Pair(5, "")), queue.priorityEnqueue(Pair(3, "")))
+    fun `test jumbo`() {
+        val example = listOf(
+            "1163751742",
+            "1381373672",
+            "2136511328",
+            "3694931569",
+            "7463417111",
+            "1319128137",
+            "1359912421",
+            "3125421639",
+            "1293138521",
+            "2311944581"
+        ).parse()
+        assertEquals(40, dijkstras(example))
         assertEquals(
-            listOf(Pair(0, ""), Pair(0, ""), Pair(3, ""), Pair(4, ""), Pair(5, "")),
-            queue.priorityEnqueue(Pair(4, ""))
+            listOf(
+                "89123",
+                "91234",
+                "12345",
+                "23456",
+                "34567",
+            ).parse(), listOf(
+                "8"
+            ).parse().jumbo()
         )
         assertEquals(
-            listOf(Pair(0, ""), Pair(0, ""), Pair(3, ""), Pair(4, ""), Pair(5, ""), Pair(6, "")),
-            queue.priorityEnqueue(Pair(6, ""))
+            listOf(
+                "11637517422274862853338597396444961841755517295286",
+                "13813736722492484783351359589446246169155735727126",
+                "21365113283247622439435873354154698446526571955763",
+                "36949315694715142671582625378269373648937148475914",
+                "74634171118574528222968563933317967414442817852555",
+                "13191281372421239248353234135946434524615754563572",
+                "13599124212461123532357223464346833457545794456865",
+                "31254216394236532741534764385264587549637569865174",
+                "12931385212314249632342535174345364628545647573965",
+                "23119445813422155692453326671356443778246755488935",
+                "22748628533385973964449618417555172952866628316397",
+                "24924847833513595894462461691557357271266846838237",
+                "32476224394358733541546984465265719557637682166874",
+                "47151426715826253782693736489371484759148259586125",
+                "85745282229685639333179674144428178525553928963666",
+                "24212392483532341359464345246157545635726865674683",
+                "24611235323572234643468334575457944568656815567976",
+                "42365327415347643852645875496375698651748671976285",
+                "23142496323425351743453646285456475739656758684176",
+                "34221556924533266713564437782467554889357866599146",
+                "33859739644496184175551729528666283163977739427418",
+                "35135958944624616915573572712668468382377957949348",
+                "43587335415469844652657195576376821668748793277985",
+                "58262537826937364893714847591482595861259361697236",
+                "96856393331796741444281785255539289636664139174777",
+                "35323413594643452461575456357268656746837976785794",
+                "35722346434683345754579445686568155679767926678187",
+                "53476438526458754963756986517486719762859782187396",
+                "34253517434536462854564757396567586841767869795287",
+                "45332667135644377824675548893578665991468977611257",
+                "44961841755517295286662831639777394274188841538529",
+                "46246169155735727126684683823779579493488168151459",
+                "54698446526571955763768216687487932779859814388196",
+                "69373648937148475914825958612593616972361472718347",
+                "17967414442817852555392896366641391747775241285888",
+                "46434524615754563572686567468379767857948187896815",
+                "46833457545794456865681556797679266781878137789298",
+                "64587549637569865174867197628597821873961893298417",
+                "45364628545647573965675868417678697952878971816398",
+                "56443778246755488935786659914689776112579188722368",
+                "55172952866628316397773942741888415385299952649631",
+                "57357271266846838237795794934881681514599279262561",
+                "65719557637682166874879327798598143881961925499217",
+                "71484759148259586125936169723614727183472583829458",
+                "28178525553928963666413917477752412858886352396999",
+                "57545635726865674683797678579481878968159298917926",
+                "57944568656815567976792667818781377892989248891319",
+                "75698651748671976285978218739618932984172914319528",
+                "56475739656758684176786979528789718163989182927419",
+                "67554889357866599146897761125791887223681299833479",
+            ).parse(), example.jumbo()
         )
     }
 
@@ -91,19 +152,7 @@ class Day14 {
             "1293138521",
             "2311944581"
         ).parse()
-
-        val example0 = listOf(
-            "1"
-        ).parse()
-        val example1 = listOf(
-            "21",
-            "81"
-        ).parse()
-        val example2 = listOf(
-            "639",
-            "521",
-            "581"
-        ).parse()
         assertEquals(40, dijkstras(example))
+        assertEquals(315, dijkstras(example.jumbo()))
     }
 }
